7db039af1d3983e1595dc4957c2252fe18a4c597
purpl3pineapple <opaloctopus1005@gmail.com>
2024-01-04 17:58:23 -0500

    adding css to project

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./app/store";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
